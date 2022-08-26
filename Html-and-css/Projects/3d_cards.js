8cc65131d51dde32f0f64733cac9cd5995b5e17d
Manish Kumar <manishkumar9782@gmail.com>
2022-08-26 17:50:04 +0530

    added Game card, movies card and Portfolio projects

---
+++ Html-and-css/Projects/3d_cards.js
@@ -0,0 +1,9 @@
+
+// in this section we will write down the java script for the 
+
+const CARDS = 10; 
+// Total 10 cards in our list.
+
+const MAX_VISIBILITY = 3;
+// maximum visiblily for any card.
+
